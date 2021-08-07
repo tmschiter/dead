6e924e5f6597e8086260bcad7da0001a3397898b
tmschiter <63077760+tmschiter@users.noreply.github.com>
2021-08-07 03:41:50 -0300

    Create roster.js

---
+++ roster.js
@@ -0,0 +1,77 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    /* EMBLEMS */
+
+    /* Shamans */
+    const shamans = document.getElementsByClassName('shaman');
+    for (let i = 0; i < shamans.length; i++) {
+        shamans[i].src = "../media/class-emblems/shaman_emblem.png";
+    }
+
+    /* Warriors */
+    const warriors = document.getElementsByClassName('warrior');
+    for (let i = 0; i < warriors.length; i++) {
+        warriors[i].src = "../media/class-emblems/warrior_emblem.png";
+    }
+
+    /* DKs */
+    const dks = document.getElementsByClassName('dk');
+    for (let i = 0; i < dks.length; i++) {
+        dks[i].src = "../media/class-emblems/deathknight_emblem.png";
+    }
+
+    /* Druids */
+    const druids = document.getElementsByClassName('druid');
+    for (let i = 0; i < druids.length; i++) {
+        druids[i].src = "../media/class-emblems/druid_emblem.png";
+    }
+
+    /* Paladins */
+    const paladins = document.getElementsByClassName('paladin');
+    for (let i = 0; i < paladins.length; i++) {
+        paladins[i].src = "../media/class-emblems/paladin_emblem.png";
+    }
+
+    /* Priests */
+    const priests = document.getElementsByClassName('priest');
+    for (let i = 0; i < priests.length; i++) {
+        priests[i].src = "../media/class-emblems/priest_emblem.png";
+    }
+
+    /* Monks */
+    const monks = document.getElementsByClassName('monk');
+    for (let i = 0; i < monks.length; i++) {
+        monks[i].src = "../media/class-emblems/monk_emblem.png";
+    }
+
+    /* Mages */
+    const mages = document.getElementsByClassName('mage');
+    for (let i = 0; i < mages.length; i++) {
+        mages[i].src = "../media/class-emblems/mage_emblem.png";
+    }
+
+    /* Warlocks */
+    const warlocks = document.getElementsByClassName('warlock');
+    for (let i = 0; i < warlocks.length; i++) {
+        warlocks[i].src = "../media/class-emblems/warlock_emblem.png";
+    }
+
+    /* Hunters */
+    const hunters = document.getElementsByClassName('hunter');
+    for (let i = 0; i < hunters.length; i++) {
+        hunters[i].src = "../media/class-emblems/hunter_emblem.png";
+    }
+
+    /* Rogues */
+    const rogues = document.getElementsByClassName('rogue');
+    for (let i = 0; i < rogues.length; i++) {
+        rogues[i].src = "../media/class-emblems/rogue_emblem.png";
+    }
+
+    /* Demon Hunters */
+    const dhs = document.getElementsByClassName('dh');
+    for (let i = 0; i < dhs.length; i++) {
+        dhs[i].src = "../media/class-emblems/demonhunter_emblem.png";
+    }
